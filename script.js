8ed2744acfb95c0e1d7e7796a442fbe6fe7446ee
unknown <hutherandrea@outlook.com>
2021-04-28 17:06:38 +0200

    21 card game wip

---
+++ script.js
@@ -0,0 +1,37 @@
+let playerTarget = document.getElementById("playerCard");
+let computerTarget = document.getElementById("cpuCard");
+let messageTarget = document.getElementById("message");
+
+let suits = ["spades", "diamonds", "clubs", "hearts"];
+let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+// function getDeck() {
+   let deck = new Array();
+
+    for (let i = 0; i < suits.length; i++) {
+        for (let x = 0; x < values.length; x++) {
+            let card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+    console.log(randomCard(deck));
+    // return deck;
+// }
+
+document.getElementById("hit").addEventListener("click", function () {
+    let someCard = randomCard(deck);
+    console.log(someCard);
+    console.log(someCard["values"]);
+playerTarget.innerHTML += randomCard(deck["values"]);
+});
+
+document.getElementById("stand").addEventListener("click", function () {
+
+});
+
+function randomCard(deck) {
+
+    return deck[Math.floor(Math.random() * deck.length)];
+}
+
+
