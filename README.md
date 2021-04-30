b510d27ce40b775c8eddad006320854157fd6e3e
unknown <hutherandrea@outlook.com>
2021-04-30 11:02:26 +0200

    fixed undefined error

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js-21-card-game
+
+most recent commit:
+- fixed "undefined" error
+- remembered that JS is case sensitive!!
--- script.js
+++ script.js
@@ -6,32 +6,32 @@ let suits = ["spades", "diamonds", "clubs", "hearts"];
 let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 // function getDeck() {
-   let deck = new Array();
+let deck = new Array();
 
-    for (let i = 0; i < suits.length; i++) {
-        for (let x = 0; x < values.length; x++) {
-            let card = {Value: values[x], Suit: suits[i]};
-            deck.push(card);
-        }
+for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+        let card = {Value: values[x], Suit: suits[i]};
+        deck.push(card);
     }
-    console.log(randomCard(deck));
-    // return deck;
+}
+console.log(randomCard(deck));
+// return deck;
 // }
 
 document.getElementById("hit").addEventListener("click", function () {
     let someCard = randomCard(deck);
     console.log(someCard);
-    console.log(someCard["values"]);
-playerTarget.innerHTML += randomCard(deck["values"]);
+    console.log(someCard["Value"]);
+    playerTarget.innerHTML += someCard.Value;
 });
 
 document.getElementById("stand").addEventListener("click", function () {
 
 });
 
-function randomCard(deck) {
+function randomCard(arr) {
 
-    return deck[Math.floor(Math.random() * deck.length)];
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
 
