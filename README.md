f1b88ba1fa6ae694fd37d243b7c05f5cfb3b07fa
unknown <hutherandrea@outlook.com>
2021-04-30 16:08:14 +0200

    added more functionality, view readme

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 most recent commit:
 - fixed "undefined" error
 - remembered that JS is case sensitive!!
+
+commit on 30/04/2021 +/- 16:00:
+- made numbers and symbols (= cards) appear on the page
+- game can now determine a win or loss
+- game restarts (= resets) when clicking "hit" again after outcome is determined
+- game adds up numbers, visually displayed on the page
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let playerTarget = document.getElementById("playerCard");
 let computerTarget = document.getElementById("cpuCard");
 let messageTarget = document.getElementById("message");
+let totalTarget = document.getElementById("totalPlayer");
+let totalCPUTarget = document.getElementById("totalCPU");
+let sum = 0;
+let cpuSum = 0;
 
-let suits = ["spades", "diamonds", "clubs", "hearts"];
+let suits = ["♠", "♡", "♣", "♢"];
 let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 // function getDeck() {
@@ -18,11 +22,31 @@ console.log(randomCard(deck));
 // return deck;
 // }
 
+
 document.getElementById("hit").addEventListener("click", function () {
     let someCard = randomCard(deck);
-    console.log(someCard);
-    console.log(someCard["Value"]);
-    playerTarget.innerHTML += someCard.Value;
+    let cpuCard = randomCard(deck);
+if (sum >= 21 || cpuSum >= 21){
+    reset()
+}
+    sum=+sum+ +someCard.Value;
+    cpuSum = +cpuSum+ +cpuCard.Value;
+
+    playerTarget.innerHTML += " "+ someCard.Value+ someCard.Suit;
+    totalTarget.innerHTML = sum;
+
+    if(sum < 21) {
+        setTimeout(function() {
+            computerTarget.innerHTML += " "+ cpuCard.Value+ cpuCard.Suit;
+            totalCPUTarget.innerHTML = cpuSum;
+            if (cpuSum >21){
+                messageTarget.innerHTML = "YOU WIN!";
+            }
+        }, 500);
+    }else if(sum > 21){
+        messageTarget.innerHTML = "YOU LOST!";
+
+    }
 });
 
 document.getElementById("stand").addEventListener("click", function () {
@@ -32,6 +56,15 @@ document.getElementById("stand").addEventListener("click", function () {
 function randomCard(arr) {
 
     return arr[Math.floor(Math.random() * arr.length)];
+}
+function reset(){
+    sum = 0;
+    cpuSum = 0;
+    messageTarget.innerHTML ="PLAY AGAIN";
+    playerTarget.innerHTML = "Your cards:";
+    computerTarget.innerHTML= "CPUs cards:";
+    totalTarget.innerHTML= "0";
+    totalCPUTarget.innerHTML= "0";
 }
 
 
